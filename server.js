ff00ceacfffff60fdb5b6f6eec9285c5601b9479
eedd3 <128992886+eedd3@users.noreply.github.com>
2026-01-29 03:59:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,3 +25,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+app.get("/", (req, res) => {
+  res.send("Backend is live! Use POST /download to test.");
+});
